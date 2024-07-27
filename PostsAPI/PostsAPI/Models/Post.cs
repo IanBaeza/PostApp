b3f1f598eca5b32d63e3f913f594b8906f924c45
Ian Baeza <ianbaezarivera@gmail.com>
2024-07-26 22:43:55 -0400

    se agrega modelo de tabla post

--- PostsAPI/PostsAPI/Models/Post.cs
+++ PostsAPI/PostsAPI/Models/Post.cs
@@ -7,8 +7,8 @@ namespace PostsAPI.Models
 {
     public class Post
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
     }
 }
