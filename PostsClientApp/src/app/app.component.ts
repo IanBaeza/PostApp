c143e6ccb1123eae98c9df700f09b00a514efaeb
Ian Baeza <ianbaezarivera@gmail.com>
2024-07-29 04:28:26 -0400

    se modifica root component de la aplicació para agregar los creado anteriormente

--- PostsClientApp/src/app/app.component.ts
+++ PostsClientApp/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import PostListaComponent from './components/post-lista/post-lista.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PostListaComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PostsClientApp/src/app/app.routes.ts
+++ PostsClientApp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/post-lista/post-lista.component')
+    }
+];
